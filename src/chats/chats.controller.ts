e9064cd8672e942262a1883cce914671e305f95b
Essien Ekanem <evidenzeekanem@gmail.com>
2023-04-20 14:19:29 +0100

    Added authentication to chat

--- src/chats/chats.controller.ts
+++ src/chats/chats.controller.ts
@@ -1,8 +1,10 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { ChatsService } from './chats.service';
 import { NewConversationDto } from './dto/chats.dto';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
 @Controller('chats')
+@UseGuards(JwtAuthGuard)
 export class ChatsController {
   constructor(private readonly chatsService: ChatsService) {}
 
